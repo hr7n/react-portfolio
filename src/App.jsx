c90eb6187baf6987497322da6cdb5d1a5ebcd2b7
mhorton <horton-michael@users.noreply.github.com>
2023-10-26 18:36:18 -0400

    first commit, set up vite & react

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div></div>
+      <h1>React Portfolio</h1>
+      <div className="card">
+        {/* <button onClick={() => setCount((count) => count + 1)}>
+          Michael Horton
+        </button> */}
+      </div>
+    </>
+  );
+}
+
+export default App;
